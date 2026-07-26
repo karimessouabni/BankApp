a0291a65f7b58b984bf3d3596d174b3447cbf126
es-souabni <karimessouabni0@gmail.com>
2026-07-26 21:05:11 +0200

    Actualizar test.tf

--- test.tf
+++ test.tf
@@ -1,15 +1,11 @@
-ws_outputs = tf_workspace.outputs()   # [SchematicsWorkspaceOutputs(...)]
-
-restore_id = None
-for out in ws_outputs:                          # un par template_data
-    for values in out.output_values:            # liste de dicts d'outputs
-        if "restore_id" in values:
-            restore_id = values["restore_id"]["value"]
-            break
-    if restore_id:
-        break
-
-if restore_id is None:
-    raise ValueError("restore_id absent des outputs du workspace")
-
-logger.info("restore_id=%s", restore_id)
+state_manager.push_state({
+    "recovery_ranges": [
+        {
+            "recovery_range_id": r["recovery_range_id"],
+            "range_start_time": r["range_start_time"],
+            "range_end_time": r["range_end_time"],
+        }
+        for r in ranges[:20]          # les N plus récents, pas tout l'historique
+    ],
+    "recovery_ranges_fetched_at": datetime.now(timezone.utc).isoformat(),
+})
