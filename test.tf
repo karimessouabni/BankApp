110129f21fa04e47eac021d23b18cdfdcf51b746
es-souabni <karimessouabni0@gmail.com>
2026-07-28 11:51:51 +0200

    Actualizar test.tf

--- test.tf
+++ test.tf
@@ -1,11 +1,3 @@
-state_manager.push_state({
-    "recovery_ranges": [
-        {
-            "recovery_range_id": r["recovery_range_id"],
-            "range_start_time": r["range_start_time"],
-            "range_end_time": r["range_end_time"],
-        }
-        for r in ranges[:20]          # les N plus récents, pas tout l'historique
-    ],
-    "recovery_ranges_fetched_at": datetime.now(timezone.utc).isoformat(),
-})
+curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=$IC_API_KEY" | jq .
