80b6e454a7e1bec20f43e1b69a7cdfd72456264a
es-souabni <karimessouabni0@gmail.com>
2026-09-01 14:51:07 +0200

    Actualizar tezt.bash

--- tezt.bash
+++ tezt.bash
@@ -1,3 +1,14 @@
+API_KEY="ton_api_key"
+COS_ENDPOINT="https://s3.eu-de.cloud-object-storage.appdomain.cloud"
+BUCKET="mon-bucket"
+
+# 1. Échanger l'API key contre un token IAM
 IAM_TOKEN=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${API_KEY}" \
-  | grep -o '"access_token":"[^"]*' | cut -d'"' -f4)
+  | jq -r .access_token)
+
+# 2. List objects
+curl -s "${COS_ENDPOINT}/${BUCKET}?list-type=2" \
+  -H "Authorization: Bearer ${IAM_TOKEN}"
+
